fed409344770349e24a334efae8c846f2a97f037
Aysun <aysunkapusuz@gmail.com>
2022-06-14 08:13:53 -0600

    more code added

--- script.js
+++ script.js
@@ -8,8 +8,15 @@ let int = setInterval(blurring, 30)
 function blurring(){
     load++
 
+    if(load>99){
+        clearInterval(int)
+    }
 
 
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = 
+}
 
-    console.log(load)
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num -in_min)*(out_max-out_min)/ (in_max-in_min) + out_min;
 }
