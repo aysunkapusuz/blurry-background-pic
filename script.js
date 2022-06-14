184063fd5b79a4da239112bd6e493254e521bc2d
Aysun <aysunkapusuz@gmail.com>
2022-06-14 08:19:09 -0600

    more code added

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const loadText = document.querySelector('.loading_text')
 const main = document.querySelector('.main')
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num -in_min)*(out_max-out_min)/ (in_max-in_min) + out_min;
+}
 
 let load = 0
 let int = setInterval(blurring, 30)
@@ -15,10 +18,7 @@ function blurring(){
 
     loadText.innerText = `${load}%`
     loadText.style.opacity = scale(load, 0, 100, 1, 0)
-    main.style.filter = `blur(${scale(load, 0, 100, 30, 0)})`
+    main.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 
 }
 
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return (num -in_min)*(out_max-out_min)/ (in_max-in_min) + out_min;
-}
