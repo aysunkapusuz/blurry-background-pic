2052f11b2a61f5c08aa94419d09474b1c1471567
Aysun <aysunkapusuz@gmail.com>
2022-06-14 08:08:44 -0600

    more code added

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const loadText = document.querySelector('.loading_text')
 const main = document.querySelector('.main')
 
 let load = 0
+let int = setInterval(blurring, 30)
+
+
+function blurring(){
+    load++
+
+
+
+
+    console.log(load)
+}
